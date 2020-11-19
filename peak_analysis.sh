e64eebe75cf97e85980e5f73f63166f30b3ca79c
Christiane <gaertner.christiane@web.de>
2020-11-19 09:57:12 +0100

    bug fixes

--- peak_analysis.sh
+++ peak_analysis.sh
@@ -8,7 +8,7 @@ for i in *_peaks_*
 do
 	bedtools intersect -a $i -b $anno_path -wa -wb > intersect_ribo_anno/${i}_intersect_ribo.bed
 done
-python3 scripts/Ribo_intersect_analysis.py -i intersect_ribo_anno -o ribo_peaks_csv
+python3 ../scripts/Ribo_intersect_analysis.py -i intersect_ribo_anno -o ribo_peaks_csv
 FILES=ribo_peaks_csv/*
 for f in $FILES
 do
