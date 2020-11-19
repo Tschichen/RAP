9fa2c03afca4ffd094579a1a40e9e7a9c571cdd9
Christiane <gaertner.christiane@web.de>
2020-11-19 09:39:26 +0100

    bug fixes

--- peak_analysis.sh
+++ peak_analysis.sh
@@ -3,12 +3,10 @@
 # $1 = directory with files out of peak finding.
 # $2 = path to ribozyme annotation
 var1=$1;var2=$2
-cd $var1
-anno="../"
-anno_path=$anno$var2
-mkdir ../intersect_ribo_anno && mkdir ../ribo_peaks_csv
-for i in *_peak_*.bed.gz; do bedtools intersect -a $i -b $anno_path -wa -wb > ../intersect_ribo_anno/${i}_intersect_ribo.bed; done
-cd ..
+peak_dir=$var1
+anno_path=$var2
+mkdir intersect_ribo_anno && mkdir ribo_peaks_csv
+for i in $peakdir/*_peak_*.bed.gz; do bedtools intersect -a $i -b $anno_path -wa -wb > intersect_ribo_anno/${i}_intersect_ribo.bed; done
 python3 scripts/Ribo_intersect_analysis.py -i intersect_ribo_anno -o ribo_peaks_csv
 FILES=ribo_peaks_csv/*
 for f in $FILES
