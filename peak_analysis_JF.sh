cfb3e171a5e5b17a5d0cf19238e350abe1c9157f
Christiane <gaertner.christiane@web.de>
2021-02-26 12:05:40 +0100

    evaluate peak caller results peakf JF

---
+++ peak_analysis_JF.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# should be executed in the RAP directory after cloning it. Directory with peak_files should be linked to this directory, too, named peaks
+# $1 = path to ribozyme annotation
+anno_path=$1
+mkdir intersect_ribo_anno
+mkdir ribo_peaks_csv
+mkdir plots
+cd peaks
+anno="../"
+anno_dir=$anno$anno_path
+for i in *; 
+do
+if [[ $i =~ "_peak_sorted" ]]
+then
+bedtools intersect -a $i -b $anno_dir -wa -wb -s > ../intersect_ribo_anno/${i}_intersect_ribo.bed
+fi
+done
+cd ..
+python3 scripts/Ribo_intersect_analysis.py -i intersect_ribo_anno -o ribo_peaks_csv -n
+cd ribo_peaks_csv
+for f in *
+do
+	name=${f##*/}
+	string1="../plots/"
+	string2="_peaks"
+	plot_name=$string1$name$string2
+	Rscript ../scripts/plot_ribo_peaks.R $f $name $plot_name
+done
